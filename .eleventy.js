a278bc76981c204529b6ca3181e36597d11b9e3f
amyspencerproject <amy@spencerproject.com>
2022-03-28 19:38:04 -0600

    Changed folder structure and added info to .eleventy.js file

--- .eleventy.js
+++ .eleventy.js
@@ -8,3 +8,17 @@
 //     // return object options in the object starting on the line below
 //     return {};
 //   };
+
+module.exports = config => {
+    // Set directories to pass through to the site folder
+    config.addPassthroughCopy('./src/assets');
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: '_site'
+        }
+    };
+};
